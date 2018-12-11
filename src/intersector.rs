14c0e2c75bced25d1a9871954fcc7755ed4dbac6
Asger <asger.nyman@alexandra.dk>
2018-12-11 10:04:50 +0100

    Create buffers in intersector

--- src/intersector.rs
+++ src/intersector.rs
@@ -4,14 +4,18 @@ use std::mem;
 
 pub struct Intersector {
     acceleration_structure: TriangleAccelerationStructure,
-    ray_intersector: RayIntersector
+    ray_intersector: RayIntersector,
+    ray_buffer: Buffer,
+    intersection_buffer: Buffer,
+    ray_count: u64
 }
 
 impl Intersector {
 
     pub fn new(device: &DeviceRef) -> Intersector
     {
-        // Acceleration structure:
+        let ray_count = 1;
+        // Triangle data
         let vertex_data = [
             0.25f32, 0.25, 0.0,
             0.75, 0.25, 0.0,
@@ -21,6 +25,7 @@ impl Intersector {
             0u32, 1, 2
         ];
 
+        // Build acceleration structure:
         let vertex_buffer = device.new_buffer_with_data( unsafe { mem::transmute(vertex_data.as_ptr()) },
                                      (vertex_data.len() * mem::size_of::<f32>()) as u64,
                                      MTLResourceOptions::CPUCacheModeDefaultCache);
@@ -36,22 +41,26 @@ impl Intersector {
         acceleration_structure.set_triangle_count(1);
         acceleration_structure.rebuild();
 
-        // Ray intersector:
+        // Setup ray intersector:
         let ray_intersector = RayIntersector::new(&device);
         ray_intersector.set_ray_stride(8 * std::mem::size_of::<f32>() as i64);
         ray_intersector.set_ray_data_type(1); // MPSRayDataTypeOriginMinDistanceDirectionMaxDistance
         ray_intersector.set_intersection_stride(8 * std::mem::size_of::<f32>() as i64);
         ray_intersector.set_intersection_data_type(4); // MPSIntersectionDataTypeDistancePrimitiveIndexCoordinates
 
-        Intersector {acceleration_structure, ray_intersector}
+        // Create buffers:
+        let ray_buffer = device.new_buffer(ray_count * 8 * std::mem::size_of::<f32>() as u64, MTLResourceOptions::StorageModePrivate);
+        let intersection_buffer = device.new_buffer(ray_count * 8 * std::mem::size_of::<f32>() as u64, MTLResourceOptions::StorageModePrivate);
+
+        Intersector {acceleration_structure, ray_intersector, ray_buffer, intersection_buffer, ray_count}
     }
 
-    pub fn encode_into(&self, command_buffer: &CommandBufferRef, ray_buffer: &BufferRef, intersection_buffer: &BufferRef, ray_count: u64)
+    pub fn encode_into(&self, command_buffer: &CommandBufferRef)
     {
         self.ray_intersector.encode_intersection_to_command_buffer(command_buffer, 0, //MPSIntersectionTypeNearest
-                                                                   ray_buffer, 0,
-                                                                   intersection_buffer, 0,
-                                                                   ray_count, &self.acceleration_structure);
+                                                                   &self.ray_buffer, 0,
+                                                                   &self.intersection_buffer, 0,
+                                                                   self.ray_count, &self.acceleration_structure);
     }
 
 }
